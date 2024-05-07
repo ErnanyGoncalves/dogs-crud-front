6e53906297348f9131be9fd4a8fffe65c9b90f55
Ernany Gonçalves <goncalves.ernany@gmail.com>
2024-05-07 15:37:41 -0300

    making some experiments with styling in the components

--- src/app/list-dogs/dog-card/dog-card.component.ts
+++ src/app/list-dogs/dog-card/dog-card.component.ts
@@ -1,12 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dog-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dog-card.component.html',
   styleUrl: './dog-card.component.scss'
 })
 export class DogCardComponent {
-
+  dog= {
+    gender:'male'
+  }
 }
--- src/app/list-dogs/dog-info-modal/dog-info-modal.component.ts
+++ src/app/list-dogs/dog-info-modal/dog-info-modal.component.ts
@@ -1,12 +1,15 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dog-info-modal',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './dog-info-modal.component.html',
   styleUrl: './dog-info-modal.component.scss'
 })
 export class DogInfoModalComponent {
-
+  dog= {
+    gender: 'male'
+  }
 }
