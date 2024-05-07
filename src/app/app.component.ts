8739e695a49cdc27ecbcd748371d301124e26c6e
Ernany Gonçalves <goncalves.ernany@gmail.com>
2024-05-07 10:24:17 -0300

    adding the components import in the standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListDogsComponent } from './list-dogs/list-dogs.component';
+import { CreateDogComponent } from './create-dog/create-dog.component';
+import { EditDogComponent } from './edit-dog/edit-dog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent,ListDogsComponent,CreateDogComponent,EditDogComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/list-dogs/list-dogs.component.ts
+++ src/app/list-dogs/list-dogs.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DogCardComponent } from './dog-card/dog-card.component';
+import { DogInfoModalComponent } from './dog-info-modal/dog-info-modal.component';
 
 @Component({
   selector: 'app-list-dogs',
   standalone: true,
-  imports: [],
+  imports: [DogCardComponent,DogInfoModalComponent],
   templateUrl: './list-dogs.component.html',
   styleUrl: './list-dogs.component.scss'
 })
