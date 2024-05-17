6af3d9e0c55c0055e783c44933251a56f707f3b0
Ernany Gonçalves <goncalves.ernany@gmail.com>
2024-05-17 11:35:00 -0300

    grid, template form (create) with validation, style adjustments

--- src/app/create-dog/create-dog.component.ts
+++ src/app/create-dog/create-dog.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DogService } from '../dog.service';
+import { Dog } from '../dog.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-dog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-dog.component.html',
   styleUrl: './create-dog.component.scss'
 })
 export class CreateDogComponent {
 
+  constructor(private dogService:DogService,private router:Router){}
+
+  validateUrl(url: string): boolean {
+    console.log("URL",url)
+    const urlPattern = /^(http|https):\/\/.*\.(jpg|png)$/;
+    return urlPattern.test(url);
+  }
+
+  onSubmit(formData: HTMLFormElement){
+    this.validateUrl(formData['value'].photo)
+    if(formData['valid']){
+    this.dogService.postDog(formData['value'] as Dog);
+    this.router.navigate(['dogs']);
+  }
+  }
 }
